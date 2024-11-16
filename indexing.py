f6630b0582e226aa674083d73ed0a20b731221c9
Cristina Teixidó Cruïlles <cristinateixido9@gmail.com>
2024-11-16 13:40:24 +0100

    Update indexing.py and sims.py

--- indexing.py
+++ indexing.py
@@ -1,22 +1,24 @@
-import subprocess  # Import subprocess
+import subprocess 
 
-
-EXTRACT_DIR = 'arxiv'
-INDEX_NAME = 'exercise2'
+directories = ['Objectives_files', 'Technical_files']
+index_names = ['objective_ind', 'technical_ind']
 
 def index_documents(script_path, index_name, extract_dir):
     print("Indexing documents...")
-    # Construct the command to run IndexFilesPreprocess.py
     command = [
         'python3', script_path,
         '--index', index_name,
         '--path', extract_dir,
         '--token', 'letter',
-        '--filter', 'lowercase', 'asciifolding'
+        '--filter', 'lowercase', 'asciifolding', 'stop', 'porter_stem'
     ]
     # Execute the script with the specified arguments
     subprocess.run(command)
     print("Indexing completed.")
 
-script_path = 'IndexFilesPreprocess.py'  # Make sure this path is correct
-index_documents(script_path, INDEX_NAME, EXTRACT_DIR)
+script_path = 'IndexFilesPreprocess.py' 
+
+for i in range(2):
+    EXTRACT_DIR = directories[i]
+    INDEX_NAME = index_names[i]
+    index_documents(script_path, INDEX_NAME, EXTRACT_DIR)
